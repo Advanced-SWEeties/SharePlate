36f445cd1b62bbb1446413b2cebf18abbae60c65
philluple <pnl2111@columbia.edu>
2024-11-26 00:47:12 -0500

    fix

--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Container from '@mui/material/Container'
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 const Layout = ({ children }) => {
-  const navigate = useNavigate()
 
   return (
     <>
