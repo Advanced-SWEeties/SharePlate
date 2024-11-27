f21e2aa3a6ed5e013f289c522c6d0b8bf2acb174
Phillip Le <101436499+philluple@users.noreply.github.com>
2024-11-27 09:49:00 -0500

    Update Rating.js

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Star from "../components/Star";
